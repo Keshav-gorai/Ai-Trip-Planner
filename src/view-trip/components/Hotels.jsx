3a5f91899a0979d56d59f14751a11723817060cd
Keshav-gorai <kumarkeshavg20@gmail.com>
2025-01-14 02:29:10 +0530

    completed

--- src/view-trip/components/Hotels.jsx
+++ src/view-trip/components/Hotels.jsx
@@ -5,10 +5,10 @@ import HotelCardItem from "./HotelCardItem";
 function Hotels({ trip }) {
   return (
     <div>
-      <h2 className="font-bold text-xl  my-6">Hotels Recommendation</h2>
+      <h2 className="font-bold text-xl my-6">Hotels Recommendation</h2>
       <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
         {trip?.tripData?.hotel_options?.map((hotel, index) => (
-         <HotelCardItem hotel= {hotel}/>
+          <HotelCardItem key={index} hotel={hotel} />
         ))}
       </div>
     </div>
--- src/view-trip/components/PlacesToVisit.jsx
+++ src/view-trip/components/PlacesToVisit.jsx
@@ -6,18 +6,20 @@ function PlacesToVisit({ trip }) {
     <div>
       <h2 className="font-bold text-lg mt-10 mb-4">Places to Visit</h2>
       <div>
-        {trip?.tripData?.itinerary?.map((item, index) => (
-          <div>
-            <h2 className="font-medium text-lg capitalize mt-5 ">
+        {trip?.tripData?.itinerary?.map((item, dayIndex) => (
+          <div key={`day-${dayIndex}`}>
+            <h2 className="font-medium text-lg capitalize mt-5">
               For {item.day}
             </h2>
             <div>
-              {item?.plan?.map((info, index) => (
-                <div>
-                  <h2 className="mb-2 font-semibold text-orange-600">Timings: {info.best_time_to_visit}</h2>
+              {item?.plan?.map((info, planIndex) => (
+                <div key={`plan-${dayIndex}-${planIndex}`}>
+                  <h2 className="mb-2 font-semibold text-orange-600">
+                    Timings: {info.best_time_to_visit}
+                  </h2>
                   <div className="grid md:grid-cols-2 gap-3">
-                    {info?.places?.map((place, index) => (
-                      <div className="my-3">
+                    {info?.places?.map((place, placeIndex) => (
+                      <div key={`place-${dayIndex}-${planIndex}-${placeIndex}`} className="my-3">
                         <PlaceCardItem place={place} />
                       </div>
                     ))}
